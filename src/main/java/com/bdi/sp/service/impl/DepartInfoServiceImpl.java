034ad862a6321a381f431489b74f2d269705c17c
KOITT <KOITT@KOITT-PC>
2018-10-16 21:10:07 +0900

    bdi-dx init

---
+++ src/main/java/com/bdi/sp/service/impl/DepartInfoServiceImpl.java
@@ -0,0 +1,47 @@
+package com.bdi.sp.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bdi.sp.dao.DepartInfoDAO;
+import com.bdi.sp.service.DepartInfoService;
+import com.bdi.sp.vo.Depart;
+
+@Service
+public class DepartInfoServiceImpl implements DepartInfoService {
+
+	@Autowired
+	private DepartInfoDAO ddao;
+	@Override
+	public List<Depart> getDepartList(Depart d) {
+		// TODO Auto-generated method stub
+		return ddao.getDepartList(d);
+	}
+
+	@Override
+	public Depart getDepart(int dino) {
+		// TODO Auto-generated method stub
+		return ddao.getDepart(dino);
+	}
+
+	@Override
+	public int insertDepart(Depart d) {
+		// TODO Auto-generated method stub
+		return ddao.insertDepart(d);
+	}
+
+	@Override
+	public int deleteDepart(int dino) {
+		// TODO Auto-generated method stub
+		return ddao.deleteDepart(dino);
+	}
+
+	@Override
+	public int updateDepart(Depart d) {
+		// TODO Auto-generated method stub
+		return ddao.updateDepart(d);
+	}
+
+}
