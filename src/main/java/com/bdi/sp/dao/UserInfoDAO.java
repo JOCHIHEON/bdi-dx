205012fac65d70e6689113f3c62840e0ef51fb13
KOITT <KOITT@KOITT-PC>
2018-10-17 21:01:28 +0900

    please..

---
+++ src/main/java/com/bdi/sp/dao/UserInfoDAO.java
@@ -0,0 +1,15 @@
+package com.bdi.sp.dao;
+
+import java.util.List;
+
+import com.bdi.sp.vo.User;
+
+public interface UserInfoDAO {
+
+	public List<User> getUserList(User ui);
+	public User getUser(int uino);
+	public int insertUser(User ui);
+	public int deleteUser(int uino);
+	public int updateUser(User ui);
+
+}
